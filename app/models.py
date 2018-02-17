32ea1751067598a99cf4387ab6fea20ab8e7c384
Argenis Osorio <argenisosorio580@hotmail.com>
2018-02-17 06:51:46 -0400

    Estables los demás campos dinámicos

--- app/models.py
+++ app/models.py
@@ -4,7 +4,14 @@ from datetime import datetime
 
 
 class Minuta(models.Model):
-    responsable = models.CharField(max_length=50)
+    responsable = models.CharField(max_length=50,blank=True,null=True)
+    asistentes = models.TextField(max_length=2000,blank=True,null=True)
+    asunto = models.CharField(max_length=200,blank=True,null=True)
+    fecha = models.CharField(max_length=50,blank=True,null=True)
+    hora_inicio = models.CharField(max_length=50,blank=True,null=True)
+    hora_final = models.CharField(max_length=50,blank=True,null=True)
+    puntos = models.TextField(max_length=2000,blank=True,null=True)
+    acuerdos = models.TextField(max_length=2000,blank=True,null=True)
 
     def __unicode__(self):
         return self.responsable
